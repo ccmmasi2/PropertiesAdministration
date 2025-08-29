987652f5f5451f2dae4540e0ea52a676205ae97e
cmontenegro <ext.cmontenegro@maxillc.com>
2025-08-29 17:03:08 -0500

    db initializer

---
+++ Properties.Solution/Properties.Infrastructure/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace Properties.Infrastructure
+{
+    public class DbInitializer
+    {
+        private readonly AppDbContext _db;
+
+        public DbInitializer(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Any())
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Migration failed", ex);
+            }
+
+            if (_db.ChangeTracker.HasChanges())
+                _db.SaveChanges();
+        }
+    }
+}
